8aad30d04a71db5f9ef07f8d75123985a14ef598
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-21 12:20:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,9 @@
 # require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library (file_path)
+  emoticons = YAML.load_file(file_path)
+  emoticons
 end
 
 def get_japanese_emoticon
