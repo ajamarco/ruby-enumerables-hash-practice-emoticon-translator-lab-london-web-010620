1e675a1ef8fdec044bda752d0ee470abb9c5ff4d
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-21 12:33:54 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -5,8 +5,6 @@ emoticons = YAML.load_file(path)
 puts emoticons
 hash_to_return = {}
 emoticons.each do |key, value|
-  puts "agora a key #{key}. adiciona-la"
-  puts "e o value #{value}"
   puts "o japanese emoticon é #{value[1]}"
   
 end
