d8bf02fffaa77b75bd6c1aaddfd966d3c420520d
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-21 12:47:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,8 +4,8 @@ require "yaml"
 def load_library (file_path)
   emoticons = YAML.load_file(file_path)
   hash_to_return = {:get_meaning => {}, :get_emoticon => {}}
-  emoticons.each_value do |value|
-    hash_to_return[:get_meaning][value[1]] = {}
+  emoticons.each do |key, value|
+    hash_to_return[:get_meaning][value[1]] = key
   end
   puts hash_to_return
   hash_to_return
