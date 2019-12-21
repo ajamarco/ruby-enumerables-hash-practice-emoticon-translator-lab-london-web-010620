933c603027089fa2567f832bc99c15fe3c95e9ab
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-21 12:49:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,6 +6,7 @@ def load_library (file_path)
   hash_to_return = {:get_meaning => {}, :get_emoticon => {}}
   emoticons.each do |key, value|
     hash_to_return[:get_meaning][value[1]] = key
+    hash_to_return[:get_emoticon][value[0]] = value[1]
   end
   puts hash_to_return
   hash_to_return
