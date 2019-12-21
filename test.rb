e728fcf0227b9eebaab5ca36cb7a9ed22da66d7d
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-21 12:27:31 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -3,3 +3,7 @@ require "yaml"
 path = "lib/emoticons.yml"
 emoticons = YAML.load_file(path)
 puts emoticons
+emoticons.each do |key, value|
+  puts "agora a key #{key}"
+  puts "e o value #{value}"
+end
